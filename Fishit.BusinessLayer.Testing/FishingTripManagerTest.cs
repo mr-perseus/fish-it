9dfadfb582a74eb465b1d4518360a4a2d627cba9
Jan Frei <janfrei01@gmail.com>
2019-03-20 14:07:45 +0100

    Added Testing Projects

---
+++ Fishit.BusinessLayer.Testing/FishingTripManagerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Fishit.BusinessLayer.Testing
+{
+    public class FishingTripManagerTest
+    {
+        [Fact]
+        public void TestCreateFishingTripTodo()
+        {
+
+        }
+    }
+}
