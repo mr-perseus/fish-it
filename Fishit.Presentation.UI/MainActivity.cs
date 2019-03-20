8d89ffe913b03571143e7abe1ce1f87250685693
Jan Frei <janfrei01@gmail.com>
2019-03-20 11:59:13 +0100

    Renamed Fishit.Presentation.UI, Removed Tests

--- Fishit.Presentation.UI/MainActivity.cs
+++ Fishit.Presentation.UI/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
-namespace Fishit
+namespace Fishit.Presentation.UI
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
