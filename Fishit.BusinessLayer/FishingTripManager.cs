574884db0c7bc8337c01796e2f802e0c6888c9b5
Jan Frei <janfrei01@gmail.com>
2019-03-31 14:31:30 +0200

    FishingTripManager

---
+++ Fishit.BusinessLayer/FishingTripManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Fishit.Common;
+using Fishit.Dal.Entities;
+
+namespace Fishit.BusinessLayer
+{
+    public class FishingTripManager
+    {
+        public IEnumerable<FishingTrip> GetListByLocation(string location)
+        {
+            return GetList().Where(trip => trip.Location == location);
+
+        }
+
+        public IEnumerable<FishingTrip> GetList()
+        {
+            return new FishingTripDao().GetList();
+        }
+
+        public void AddFishingTrip(FishingTrip fishingTrip)
+        {
+            new FishingTripDao().AddFishingTrip(fishingTrip);
+        }
+    }
+}
