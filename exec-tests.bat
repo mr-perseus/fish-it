9071c5c1dd6d592414a241804d788c53027f8357
Jan Frei <janfrei01@gmail.com>
2019-04-01 12:19:01 +0200

    Updated batch scripts

--- exec-tests.bat
+++ exec-tests.bat
@@ -1,3 +1,3 @@
-dotnet test Fishit.BusinessLayer.Testing /p:CollectCoverage=true /p:CoverletOutputFormat=opencover -l:trx;LogFileName=TestOutput.xml
-dotnet test Fishit.Common.Testing /p:CollectCoverage=true /p:CoverletOutputFormat=opencover -l:trx;LogFileName=TestOutput.xml
-dotnet test Fishit.Presentation.Testing /p:CollectCoverage=true /p:CoverletOutputFormat=opencover -l:trx;LogFileName=TestOutput.xml
+dotnet test Fishit.BusinessLayer.Testing --collect:"Code Coverage" -l:trx;LogFileName=TestOutput.xml
+dotnet test Fishit.Common.Testing --collect:"Code Coverage" -l:trx;LogFileName=TestOutput.xml
+dotnet test Fishit.Presentation.Testing --collect:"Code Coverage" -l:trx;LogFileName=TestOutput.xml
