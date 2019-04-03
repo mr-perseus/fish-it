6121b1a3c49fd2532cd566dd129860fd6c829dff
jan <janfrei01@gmail.com>
2019-04-03 12:00:02 +0200

    fixed test script

--- Client/exec-tests.bat
+++ Client/exec-tests.bat
@@ -1,3 +1,5 @@
+cd Client
 dotnet test Fishit.BusinessLayer.Testing --collect:"Code Coverage" -l:trx;LogFileName=TestOutput.xml
 dotnet test Fishit.Common.Testing --collect:"Code Coverage" -l:trx;LogFileName=TestOutput.xml
 dotnet test Fishit.Presentation.Testing --collect:"Code Coverage" -l:trx;LogFileName=TestOutput.xml
+cd ..
