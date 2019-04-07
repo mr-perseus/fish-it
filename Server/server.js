56e870e9894bcf07bdb221d5b47d836c29c4bbc9
jan <janfrei01@gmail.com>
2019-04-07 14:45:37 +0200

    Added log4js with config

--- Server/server.js
+++ Server/server.js
@@ -3,11 +3,53 @@ const express = require("express")
 const server = express()
 const config = require("config")
 const mongoose = require("mongoose")
+const log4js = require("log4js");
 
 const auth = require("./routes/authRoutes")
 const users = require("./routes/usersRoutes")
 const friends = require("./routes/friendsRoutes")
 const fishingTrips = require("./routes/fishingTripsRoutes")
+
+log4js.configure({
+	appenders: {
+		// stdout: {type: "stdout"},
+		stderr: {
+			type: "stderr",
+			layout: {
+				type: "pattern",
+				pattern: "%[[%d]; [%p]; %c; %z; %h;%] %m;%n",
+			},
+		},
+		errors: {
+			type: "file", filename: "..\\logs\\error.log", maxLogSize: 10485760, backups: 10,
+			layout: {
+				type: "pattern",
+				pattern: "[%d]; [%p]; %c; %z; %h; %m;",
+			},
+		},
+		infos: {
+			type: "file", filename: "..\\logs\\info.log", maxLogSize: 10485760, backups: 10,
+			layout: {
+				type: "pattern",
+				pattern: "[%d]; [%p]; %c; %z; %h; %m;",
+			},
+		},
+		debugs: {
+			type: "file", filename: "..\\logs\\debug.log", maxLogSize: 10485760, backups: 10,
+			layout: {
+				type: "pattern",
+				pattern: "[%d]; [%p]; %c; %z; %h; %m;",
+			},
+		},
+		fileFilterError: { type: "logLevelFilter", appender: "errors", level: "ERROR" },
+		fileFilterInfo: { type: "logLevelFilter", appender: "infos", level: "INFO" },
+		fileFilterDebug: { type: "logLevelFilter", appender: "debugs", level: "DEBUG" },
+		stderrFilter: { type: "logLevelFilter", appender: "stderr", level: "ERROR" },
+	},
+	categories: {
+		default: { appenders: ["fileFilterError", "fileFilterInfo", "fileFilterDebug", "stderrFilter"], level: "DEBUG" },
+	},
+});
 
 // IMPORTANT: set your json web token private key as an environmental variable.
 if (!config.get("jwtPrivateKey")) {
