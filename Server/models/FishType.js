c89385c418054c451cd16f5ec2bce1d425dbd952
Maroben <maroben@daishen.ch>
2019-04-11 17:05:05 +0100

    fishType API

---
+++ Server/models/FishType.js
@@ -0,0 +1,33 @@
+const Joi = require("joi")
+const mongoose = require("mongoose")
+
+const fishTypeSchema = new mongoose.Schema({
+	Name: {
+		type: String,
+		required: true,
+		maxlength: 64
+	},
+	Description: {
+		type: String,
+		maxlength: 1024
+	}
+})
+
+const FishType = mongoose.model("FishTypes", fishTypeSchema)
+
+const fishTypeAttr = ["_id", "Name", "Description"]
+
+const fishTypeJoi = {
+	Name: Joi.string()
+		.max(64)
+		.required(),
+	Description: Joi.string().max(1024)
+}
+
+const validateFishType = (f) => Joi.validate(f, fishTypeJoi)
+
+module.exports = {
+	FishType,
+	fishTypeAttr,
+	validateFishType
+}
