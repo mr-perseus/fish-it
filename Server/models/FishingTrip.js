2c4f6f60f9ea1a48f83d95988f7860f28eb1111f
Maroben <maroben@daishen.ch>
2019-04-11 17:05:14 +0100

    adjustments

--- Server/models/FishingTrip.js
+++ Server/models/FishingTrip.js
@@ -2,68 +2,62 @@ const Joi = require("joi")
 const mongoose = require("mongoose")
 
 const fishingTripSchema = new mongoose.Schema({
-	weather: {
+	PredominantWeather: {
 		type: String,
 		required: true,
 		maxlength: 64
 	},
-	location: {
+	Location: {
 		type: String,
 		required: true,
 		maxlength: 64
 	},
-	date: {
+	DateTime: {
 		type: String,
 		required: true
 	},
-	description: {
+	Description: {
 		type: String,
 		maxlength: 1024
 	},
-	temperature: {
+	Temperature: {
 		type: String,
 		maxlength: 8
 	},
-	catches: {
-		type: Array
-	}
+	Catches: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Catches"
+		}
+	]
 })
 
 const FishingTrip = mongoose.model("FishingTrips", fishingTripSchema)
 
-function validateFishingTrip(fishingTrip) {
-	const equipmentsJoi = {
-		type: Joi.string().max(64),
-		name: Joi.string().max(128)
-	}
+const fishingTripAttr = [
+	"_id",
+	"PredominantWeather",
+	"Location",
+	"DateTime",
+	"Description",
+	"Temperature",
+	"Catches"
+]
 
-	const catchesJoi = {
-		fish: Joi.string()
-			.max(64)
-			.required(),
-		weight: Joi.string().max(16),
-		length: Joi.string().max(8),
-		datetime: Joi.string().length(16),
-		equipments: Joi.array().items(equipmentsJoi)
-	}
-
-	const fishingTripJoi = {
-		weather: Joi.string()
-			.max(64)
-			.required(),
-		location: Joi.string()
-			.max(64)
-			.required(),
-		date: Joi.string().required(),
-		description: Joi.string().max(1024),
-		temperature: Joi.string().max(8),
-		catches: Joi.array().items(catchesJoi)
-	}
-
-	return Joi.validate(fishingTrip, fishingTripJoi)
+const fishingTripJoi = {
+	PredominantWeather: Joi.string()
+		.max(64)
+		.required(),
+	Location: Joi.string()
+		.max(64)
+		.required(),
+	DateTime: Joi.string().required(),
+	Description: Joi.string().max(1024),
+	Temperature: Joi.string().max(8),
+	Catches: Joi.array()
 }
 
-const fishingTripAttr = ["weather", "location", "date", "description", "temperature", "catches"]
+const validateFishingTrip = (fT) => Joi.validate(fT, fishingTripJoi)
 
 module.exports = {
 	FishingTrip,
