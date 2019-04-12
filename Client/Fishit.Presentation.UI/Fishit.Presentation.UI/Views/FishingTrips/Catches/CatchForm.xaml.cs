0c1be95491755021ea2a45410e21363579008211
IvanDavidHuzanic <ihuzanic@hsr.ch>
2019-04-12 12:21:49 +0200

    Catches Form and .cs Base

--- Client/Fishit.Presentation.UI/Fishit.Presentation.UI/Views/FishingTrips/Catches/CatchForm.xaml.cs
+++ Client/Fishit.Presentation.UI/Fishit.Presentation.UI/Views/FishingTrips/Catches/CatchForm.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Threading.Tasks;
+using Fishit.Dal.Entities;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Fishit.Presentation.UI.Views.FishingTrips.Catches
@@ -6,9 +9,28 @@ namespace Fishit.Presentation.UI.Views.FishingTrips.Catches
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CatchForm : ContentPage
     {
+     //   public CatchForm() : this(new Catch()) { }
         public CatchForm()
         {
             InitializeComponent();
+           
+        }
+
+        private async Task SaveCatch()
+        {
+            await DisplayAlert("Catch Entry", "Saved Successfully", "Ok");
+        }
+
+        private async void CancelCatchForm_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private async void SaveCatch_OnClicked(object sender, EventArgs e)
+        {
+            await SaveCatch();
+            await Navigation.PopAsync();
         }
     }
 }
+
