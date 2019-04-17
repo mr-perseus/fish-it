dc99914c764f1bf9d2ca6540a6638cdf48e93ff7
Michel Bongard <spritfire73@gmail.com>
2019-04-17 10:08:39 +0200

    minor fixes

--- Client/Fishit.Dal/Entities/FishingTrip.cs
+++ Client/Fishit.Dal/Entities/FishingTrip.cs
@@ -18,7 +18,7 @@ namespace Fishit.Dal.Entities
         public string Id { get; set; } = "0";
         public string Location { get; set; }
         public DateTime DateTime { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = "";
         public Weather PredominantWeather { get; set; }
         public double Temperature { get; set; }
         public List<Catch> Catches { get; set; } = new List<Catch>();
