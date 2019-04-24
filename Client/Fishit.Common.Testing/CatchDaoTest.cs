5f2568ce6d3b993a0eeb91ea87e05141296460b4
Maroben <maroben@daishen.ch>
2019-04-24 12:09:16 +0200

    FishTypeDao

--- Client/Fishit.Common.Testing/CatchDaoTest.cs
+++ Client/Fishit.Common.Testing/CatchDaoTest.cs
@@ -9,6 +9,7 @@ namespace Fishit.Common.Testing
     public class CatchDaoTest
     {
         private CatchDao _catchDao;
+        private Response<Catch> _responseCreate;
 
         private CatchDao CatchDao =>
             _catchDao ?? (_catchDao = new CatchDao());
@@ -27,23 +28,25 @@ namespace Fishit.Common.Testing
             Weight = 15.8
         };
 
-        private readonly Catch _bCatch = new Catch
-        {
-            DateTime = new DateTime(2017, 12, 03),
-            FishType = new FishType
-            {
-                Id = "5cb34f29500b0509f4244306",
-                Name = "Sea Urchin",
-                Description = "A great whatever it is"
-            },
-            Length = 866.2,
-            Weight = 175.2
-        };
-
         [Fact]
         public async void CreateCatch()
         {
-            Response<Catch> response = await CatchDao.CreateCatch(_aCatch);
+            _responseCreate = await CatchDao.CreateCatch(_aCatch);
+            Assert.True(_responseCreate.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async void UpdateCatch()
+        {
+            _responseCreate.Content.Length = 888;
+            Response<Catch> response = await CatchDao.UpdateCatch(_responseCreate.Content);
+            Assert.True(response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async void DeleteCatch()
+        {
+            Response<Catch> response = await CatchDao.DeleteCatch(_responseCreate.Content);
             Assert.True(response.StatusCode == HttpStatusCode.OK);
         }
 
--- Client/Fishit.Common/FishTypeDao.cs
+++ Client/Fishit.Common/FishTypeDao.cs
@@ -1,15 +1,173 @@
-﻿using Fishit.Logging;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Fishit.Dal.Entities;
+using Newtonsoft.Json;
 
 namespace Fishit.Common
 {
-    internal class FishTypeDao
+    public class FishTypeDao
     {
-        private const string EndPointUri = "http://sinv-56038.edu.hsr.ch:40007/api/fishingtrips";
-        private readonly ILogger _logger;
+        private const string EndPointUri = "http://sinv-56038.edu.hsr.ch:40007/api/fishtypes";
 
-        public FishTypeDao()
+        public async Task<Response<List<FishType>>> GetAllFishTypes()
         {
-            _logger = LogManager.GetLogger(nameof(FishTypeDao));
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(EndPointUri))
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        return new Response<List<FishType>>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Unsuccessful Get All FishTypes",
+                            Content = new List<FishType>()
+                        };
+                    using (HttpContent content = response.Content)
+                    {
+                        string fishTypeContent = await content.ReadAsStringAsync();
+                        return new Response<List<FishType>>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Successful GetAllCatches",
+                            Content = ParseListFishTypes(fishTypeContent)
+                        };
+                    }
+                }
+            }
+        }
+
+        public async Task<Response<FishType>> GetFishtype()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(EndPointUri))
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        return new Response<FishType>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Unsuccessful GetAllCatches",
+                            Content = new FishType()
+                        };
+                    using (HttpContent content = response.Content)
+                    {
+                        string fishTypeContent = await content.ReadAsStringAsync();
+                        return new Response<FishType>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Successful GetAllCatches",
+                            Content = ParseFishType(fishTypeContent)
+                        };
+                    }
+                }
+            }
+        }
+
+        public async Task<Response<FishType>> CreateFishType(FishType fishType)
+        {
+            StringContent body = new StringContent(StringifyFishType(fishType), Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.PostAsync(EndPointUri + "/new", body))
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        return new Response<FishType>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Unsuccessful GetCatch",
+                            Content = new FishType()
+                        };
+                    using (HttpContent responseContent = response.Content)
+                    {
+                        string fishTypeContent = await responseContent.ReadAsStringAsync();
+                        return new Response<FishType>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Successful GetCatch",
+                            Content = ParseFishType(fishTypeContent)
+                        };
+                    }
+                }
+            }
+        }
+
+        public async Task<Response<FishType>> UpdateCatch(FishType fishType)
+        {
+            StringContent body = new StringContent(StringifyFishType(fishType), Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.PutAsync(EndPointUri + "/" + fishType.Id, body))
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        return new Response<FishType>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Unsuccessful GetCatch",
+                            Content = new FishType()
+                        };
+                    using (HttpContent responseContent = response.Content)
+                    {
+                        string fishTypeContent = await responseContent.ReadAsStringAsync();
+                        return new Response<FishType>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Successful GetCatch",
+                            Content = ParseFishType(fishTypeContent)
+                        };
+                    }
+                }
+            }
+        }
+
+        public async Task<Response<FishType>> DeleteCatch(FishType fishType)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.DeleteAsync(EndPointUri + "/" + fishType.Id))
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        return new Response<FishType>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Unsuccessful GetCatch",
+                            Content = new FishType()
+                        };
+                    using (HttpContent responseContent = response.Content)
+                    {
+                        string fishTypeContent = await responseContent.ReadAsStringAsync();
+                        return new Response<FishType>
+                        {
+                            StatusCode = response.StatusCode,
+                            Message = "Successful GetCatch",
+                            Content = ParseFishType(fishTypeContent)
+                        };
+                    }
+                }
+            }
+        }
+
+        public List<FishType> ParseListFishTypes(string fishTypes)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            { ContractResolver = new CustomContractResolver() };
+            return JsonConvert.DeserializeObject<List<FishType>>(fishTypes, settings);
+        }
+
+        public FishType ParseFishType(string fishType)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            { ContractResolver = new CustomContractResolver() };
+            return JsonConvert.DeserializeObject<FishType>(fishType, settings);
+        }
+
+        public string StringifyFishType(FishType fishType)
+        {
+            return JsonConvert.SerializeObject(fishType);
         }
     }
 }
