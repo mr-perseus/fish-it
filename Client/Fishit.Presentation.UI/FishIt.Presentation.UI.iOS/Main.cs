5af08718eecf6e503e46807ecf9304cd8e2c081b
jan <janfrei01@gmail.com>
2019-04-30 13:42:17 +0200

    ios code smell

--- Client/Fishit.Presentation.UI/FishIt.Presentation.UI.iOS/Main.cs
+++ Client/Fishit.Presentation.UI/FishIt.Presentation.UI.iOS/Main.cs
@@ -2,7 +2,7 @@
 
 namespace FishIt.iOS
 {
-    public class Application
+    public static class Application
     {
         // This is the main entry point of the application.
         private static void Main(string[] args)
