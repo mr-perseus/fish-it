761650792f27b8eeeda22eb0cb3a382f0b53b64b
Dijan Helbling <maroben@daishen.ch>
2019-05-11 21:25:07 +0200

    ..port..config

--- Server/server.js
+++ Server/server.js
@@ -29,7 +29,7 @@ server.use(logger)
 const routes = require("./routes/routes")
 server.use(["/api/test", "/api"], routes)
 
-const port = 3802
+const port = config.port
 server.listen(port, (error) => {
 	if (error) throw error
 	console.info(`ready at port ${port}`)
