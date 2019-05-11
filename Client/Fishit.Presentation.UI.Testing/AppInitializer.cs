e7dc965f1e9d7b9174244e9bffd4de72e93003bc
Jan Frei <janfrei01@gmail.com>
2019-05-11 20:04:39 +0200

    added testing framework

---
+++ Client/Fishit.Presentation.UI.Testing/AppInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Fishit.Presentation.UI.Testing
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                //return ConfigureApp.Android.StartApp();
+                return ConfigureApp.Android.ApkFile("../../../Fishit.Presentation.UI/Fishit.Presentation.UI.Android/bin/Release/ch.fishit.apk").StartApp();
+
+            }
+            
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ Client/Fishit.Presentation.UI.Testing/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Fishit.Presentation.UI.Testing
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        private IApp _app;
+        private readonly Platform _platform;
+
+        public Tests(Platform platform)
+        {
+            _platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _app = AppInitializer.StartApp(_platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            AppResult[] results = _app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            _app.Screenshot("Welcome screen.");
+
+            Assert.IsTrue(results.Any());
+        }
+    }
+}
